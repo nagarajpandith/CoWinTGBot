0297992b20b9695465c9008eb4922305b92b8e17
Nagaraj Pandith <nagarajpandith02@gmail.com>
2021-05-20 08:32:23 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,8 @@
 
 import os
 
-# this is the bot token, gotten from BotFather
-TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
-# this is your own telegram ID, which is used to send exception tracebacks
-DEVELOPER_CHAT_ID = os.environ["DEVELOPER_CHAT_ID"]
-# list of telegram IDs for them /stats command is enabled
+TELEGRAM_BOT_TOKEN = os.environ.get("TOKEN")
+DEVELOPER_CHAT_ID = os.environ.get("SUDO")
 MAINTAINERS_CHAT_IDS = [
     DEVELOPER_CHAT_ID,
 ]
